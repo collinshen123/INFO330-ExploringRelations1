35b5fcd6a7fae4e99bea7ae2b3e228a9e1044158
collinshen123 <58667267+collinshen123@users.noreply.github.com>
2023-04-06 15:09:12 -0700

    Update extra-1.sql

---
+++ extra-1.sql
@@ -0,0 +1,3 @@
+select age from persons where gender = "Female" and title  = "Dr" and city = "West Palm Beach" and state = "Florida";
+select age from persons where gender = "Male" and title  = "Rev" and city = "Phoenix" and state = "Arizona";
+-- the ages are different between the 2 people. the female has an age of 49 and the male has an age of 101.
